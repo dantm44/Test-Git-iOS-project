8f8f902fab094748e1951a11a59fa56d02f72f64
Daniel Springer <springdl@DSMP.local>
2014-08-29 13:23:01 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by Daniel Springer on 8/29/14.
+//  Copyright (c) 2014 DS Digital Media, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by Daniel Springer on 8/29/14.
+//  Copyright (c) 2014 DS Digital Media, LLC. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
